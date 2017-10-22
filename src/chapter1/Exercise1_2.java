71cfedaf95ebaac55fc495b8331afcec868d7718
xieyuzhong <xieyuzhong@haizhi.com>
2017-10-22 23:16:57 +0800

    1.2

---
+++ src/chapter1/Exercise1_2.java
@@ -0,0 +1,9 @@
+package chapter1;
+
+public class Exercise1_2 {
+    public static void main (String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Welcome to Java.");
+        }
+    }
+}
