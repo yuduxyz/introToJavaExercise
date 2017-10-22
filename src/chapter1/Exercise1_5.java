88daf1a938ad6f9913a611311b7a5f61549da982
xieyuzhong <xieyuzhong@haizhi.com>
2017-10-22 23:20:15 +0800

    1.5

---
+++ src/chapter1/Exercise1_5.java
@@ -0,0 +1,8 @@
+package chapter1;
+
+public class Exercise1_5 {
+    public static void main (String[] args) {
+        double res = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+        System.out.println(res);
+    }
+}
