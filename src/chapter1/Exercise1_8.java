9eba8e07be0c073df77ed257ae992c3cf3a84dac
xieyuzhong <xieyuzhong@haizhi.com>
2017-10-22 23:26:50 +0800

    1.8

---
+++ src/chapter1/Exercise1_8.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class Exercise1_8 {
+    public static void main (String[] args) {
+        double r = 5.5;
+        double perimeter = 2 * r * Math.PI;
+        double area = r * r * Math.PI;
+        System.out.println("perimeter is " + perimeter);
+        System.out.println("area is " + area);
+    }
+}
